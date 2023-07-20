fefd6d5f841c78c6d5a3da9e7cc5a6ed2fcf88c0
Vlad Velicu <vlad.velicu@couchbase.com>
2023-07-20 15:14:20 +0100

    update script and package dependency lookup

--- ReleaseVerify/ReleaseVerify-SPM-EE/Package.swift
+++ ReleaseVerify/ReleaseVerify-SPM-EE/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ReleaseVerify-SPM-EE",
     dependencies: [
-        .package(url: "https://github.com/couchbase/couchbase-lite-swift-ee", exact: "3.0.2")
+        .package(url: "https://github.com/couchbase/couchbase-lite-swift-ee", branch: "new-release")
     ],
     targets: [
         .executableTarget(
