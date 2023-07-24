84ba91289b40aebac783febf9de2d5a9072898bc
Vlad Velicu <vlad_vel@yahoo.com>
2023-07-24 18:08:26 +0100

    CBL-3100: Update to use spm verification for new releases (#6)
    * tests update
    
    * add -b flag for specific branch

--- ReleaseVerify/ReleaseVerify-SPM-CE/Package.swift
+++ ReleaseVerify/ReleaseVerify-SPM-CE/Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "ReleaseVerify-SPM-CE",
+    // platforms specified for Helium as XCode 14.2 defaults to ios 11 and macos 10.13 which is incompatible with product being ios 11 and macos 10.14
+    platforms: [
+        .iOS(.v11), .macOS(.v10_14)
+    ],
     dependencies: [
         .package(url: "https://github.com/couchbase/couchbase-lite-ios.git", exact: "3.0.2")
     ],
--- ReleaseVerify/ReleaseVerify-SPM-EE/Package.swift
+++ ReleaseVerify/ReleaseVerify-SPM-EE/Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "ReleaseVerify-SPM-EE",
+    // platforms specified for Helium as XCode 14.2 defaults to ios 11 and macos 10.13 which is incompatible with product being ios 11 and macos 10.14
+    platforms: [
+        .iOS(.v11), .macOS(.v10_14)
+    ],
     dependencies: [
         .package(url: "https://github.com/couchbase/couchbase-lite-swift-ee", exact: "3.0.2")
     ],
